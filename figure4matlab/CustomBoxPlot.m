65c1a108901e496e54b14733e0d70bcd8b2d629c
Valerio <valerio.modugno@gmail.com>
2016-07-19 19:21:12 +0200

    updated customboxplot for the new data input
    more comment added

--- figure4matlab/CustomBoxPlot.m
+++ figure4matlab/CustomBoxPlot.m
@@ -1,26 +1,45 @@
-clear all 
+clear variables 
 close all 
 clc
 
+
+
 %% global variable
-variable_to_plot = 'G_best_difference_performance';
-box_to_plot_selector =[2 3 6]; % this vector specify wich element i want to plot from variable_to_plot
-labels = {'g07','g09','HB'};
-colors = ['y','m','b']; % define the color of each box (inverse order)
+variable_to_plot = 'metric2';
+% title of the image
+titl = 'Constraint violations';
+title_dimension = 20;
+label_dimension = 15;
+% y axis label
+y_lab = 'sum of the constraints violations of the best solution';
+% this vector specify wich element i want to plot from variable_to_plot
+box_to_plot_selector =[1 2];
+name_file = 'RP_humanoid_bench_lbrsimple_RP_humanoid_bench_lbrsimple_more_constrained_';
+labels = {'robotics exp 1','robotics experiment 2'};
+colors = ['y','m','g']; % define the color of each box (inverse order)
+trasparency = 0.8;
 widths = 0.5; % width of the box
 space_between_box_in_a_group = 0.2;
-space_between_groups =widths + 0.5;
+space_between_groups =widths + 0.9;
+% this parameters tune when we consider a box small. It is usefull when we
+% want to apply a box even if the box is too thin to show the color
+treshold_for_small_box = 0.001;
+% i define with extension the value that i need to show the color of the
+% box if the box is too small to show the color
+extens = 0.005;
 %% path to dat file to open
 %%  SAVE PATH
  % parameter
  folder = 'benckmark';
- subfolder = {'CMAES-vanilla','CMAES-adaptive','(1+1)CMAES-vanilla'};
+ %% IMP!!! this vector represents the order in which the istogram related to the i-th method is showed in the graph
+ subfolder = {'(1+1)CMAES-vanilla','CMAES-vanilla','CMAES-adaptive'};
  allpath=which('FindData.m');
  local_path=fileparts(allpath);
  
+ 
  %% LOAD DATA
  for i=1:length(subfolder)
-    cur_mat = strcat(local_path,'/',folder,'/',subfolder{i},'/dat.mat');
+    cur_mat = strcat(local_path,'/',folder,'/',subfolder{i},'/',name_file,'.mat');
     load(cur_mat,variable_to_plot);
     store_data{i} = eval(variable_to_plot);
  end
@@ -56,10 +75,17 @@ x = [];
 group = [];
 for i=1:len_box_to_plot_selector
    for j = 1:len_store_data
-      x = [x store_data{j}(:,box_to_plot_selector(i))'];
+      x = [x store_data{j}{box_to_plot_selector(i)}];
       group = [group , index * ones(1,size(store_data{j},1))];
       index = index + 1;
    end
+end
+
+%% color box plot
+% build the complete color vector
+color_vec = [];
+for i = 1:len_box_to_plot_selector
+   color_vec = [color_vec colors];
 end
 
 %% plot boxplot
@@ -77,17 +103,16 @@ set(gca,'xtick',mean_pos)
 set(gca,'xticklabel',labels)
 
 
-%% color box plot
-% build the complete color vector
+% %% color box plot
+% % build the complete color vector
 color_vec = [];
 for i = 1:len_box_to_plot_selector
    color_vec = [color_vec colors];
 end
 
-% i define with extension the value that i need to show the color of the
-% box if the box is too small to show the color
-extens = 0;
+
 h = findobj(gca,'Tag','Box');
+h =  flipud(h);
 for j=1:length(h)
    x = get(h(j),'XData');
    y = get(h(j),'YData');
@@ -95,15 +120,17 @@ for j=1:length(h)
    x = x(1:4);
    y = y(1:4);
    
-   if(abs(y(2)-y(1))<10)
+   if(abs(y(2)-y(1))<treshold_for_small_box)
       ymean = (y(2)+y(1))/2;
       y = [ymean - extens, ymean + extens,ymean + extens, ymean - extens];
    end
-   patch(x,y,color_vec(j),'FaceAlpha',.8);
+   patch(x,y,color_vec(j),'FaceAlpha',trasparency,'LineStyle','none');
    
    
 end
 
 %% put the legend
-hleg1 = legend('(1+1)CMAES Adapt. Cov.','CMAES adaptive','CMAES vanilla' );
+hleg1 = legend('(1+1)CMAES ad. cov.','CMAES vanilla','CMAES adaptive');%,'fmincon sqd');
+ylabel(y_lab,'FontSize', label_dimension);
+title(titl,'FontSize', title_dimension);
 
